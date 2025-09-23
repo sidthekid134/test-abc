aaab3dc36a5970456c79e29dfebf6f35d54e62d0
Luv Jain <luv.jain01@gmail.com>
2025-09-22 23:47:10 -0400

    Implement: Allow saving of users
    Users should be able to persist; Application should run; Users can sign in using email and password

---
+++ app/schemas.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+from datetime import datetime
+
+
+class UserBase(BaseModel):
+    email: EmailStr
+
+
+class UserCreate(UserBase):
+    password: str = Field(..., min_length=8)
+
+
+class UserLogin(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    created_at: datetime
+    
+    class Config:
+        orm_mode = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: Optional[str] = None
